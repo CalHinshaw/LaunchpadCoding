a682ec4fd52678d11483b89df5a3a454657f175f
Cal Hinshaw <calhinshaw@gmail.com>
2016-12-11 10:43:42 -0800

    webpack works

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,27 @@
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+
 module.exports = {
   entry: "./web/static/js/app.js",
   output: {
-    path: "./priv/static/js",
-    filename: "app.js"
+    path: "./priv/static",
+    filename: "js/app.js"
   },
   resolve: {
     modulesDirectories: [ "node_modules", __dirname + "/web/static/js" ]
   },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: "babel",
-      query: {
-        presets: ["es2015"]
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel",
+        query: {
+          presets: ["es2015"]
+        }
       }
-    }]
-  }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([{ from: "./web/static/assets" }, { from: "./web/static/css", to: 'css/' }])
+  ]
 };
