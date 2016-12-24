04547e51706fc31517e13585653fd96766ad6166
CalHinshaw <calhinshaw@gmail.com>
2016-12-24 03:24:34 -0800

    fix prev and next skill fields

--- web/gql/resolvers/skill_resolver.ex
+++ web/gql/resolvers/skill_resolver.ex
@@ -2,16 +2,15 @@ defmodule Launchpad.Resolvers.Skill do
   alias Launchpad.Repo
   alias Launchpad.Models.Skill
 
-  def find(id, _info \\ nil) do
+  def find(id, _info) do
     {:ok, Repo.get(Skill, id)}
   end
 
-  def all(_args \\ nil, _info \\ nil) do
-    IO.puts Repo.all(Skill)
+  def all(_args, _info) do
     {:ok, Repo.all(Skill)}
   end
 
-  def next_skills(skill, _ \\ nil, _ \\ nil) do
+  def next_skills(skill, _, _) do
     skills = skill
     |> Ecto.assoc(:next_skills)
     |> Repo.all
@@ -19,7 +18,7 @@ defmodule Launchpad.Resolvers.Skill do
     {:ok, skills}
   end
 
-  def prev_skills(skill, _ \\ nil, _ \\ nil) do
+  def prev_skills(skill, _, _) do
     skills = skill
     |> Ecto.assoc(:prev_skills)
     |> Repo.all
--- web/gql/types.ex
+++ web/gql/types.ex
@@ -6,11 +6,11 @@ defmodule Launchpad.Schema.Types do
     field :description, :string
 
     field :next_skills, list_of(:skill) do
-      resolve &Launchpad.Resolvers.Skill.next_skills/2
+      resolve &Launchpad.Resolvers.Skill.next_skills/3
     end
 
     field :prev_skills, list_of(:skill) do
-      resolve &Launchpad.Resolvers.Skill.prev_skills/2
+      resolve &Launchpad.Resolvers.Skill.prev_skills/3
     end
   end
 
