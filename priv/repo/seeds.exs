8b1acad78d57e2348dca4f5197e4774d3edacda1
CalHinshaw <calhinshaw@gmail.com>
2016-12-24 03:01:15 -0800

    add seeds file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,26 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Launchpad.Repo
+alias Launchpad.Models.Skill
+alias Launchpad.Models.SkillArrow
+alias Launchpad.Models.User
+
+tmp = User.registration_changeset(%User{}, %{email: "admin@dev.com", password: "password", is_admin: true})
+admin_user = Repo.insert!(tmp)
+
+tmp = Skill.changeset(%Skill{}, %{name: "Hello World", description: "Write a simple program."})
+hello_world_skill = Repo.insert!(tmp)
+
+tmp = Skill.changeset(%Skill{}, %{name: "Variables", description: "Make your programs 'remeber' things."})
+variables_skill = Repo.insert!(tmp)
+
+tmp = SkillArrow.changeset(
+  %SkillArrow{},
+  %{
+    prev_skill_id: hello_world_skill.id,
+    next_skill_id: variables_skill.id
+  }
+)
+hello_world_variable_skill_arrow = Repo.insert!(tmp)
