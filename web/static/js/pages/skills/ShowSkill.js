fe651ad7de90c6900244463c12fa5037fdcae1a7
CalHinshaw <calhinshaw@gmail.com>
2016-12-25 16:06:59 -0800

    running js-interpreter

--- web/static/js/pages/skills/ShowSkill.js
+++ web/static/js/pages/skills/ShowSkill.js
@@ -8,11 +8,22 @@ import AceEditor from 'react-ace';
 import 'brace/mode/javascript';
 import 'brace/theme/tomorrow';
 
+import Interpreter from 'js-interpreter';
+
+
 function onChange(newValue) {
-  console.log('change', newValue);
+  //const myInterpreter = new Interpreter(newValue);
+  //console.log(myInterpreter);
 }
 
 class ShowSkill extends React.Component {
+  onRun() {
+    const code = this.refs.editor.editor.getValue();
+    const curInterpreter = new Interpreter(code);
+    curInterpreter.run();
+    console.log(curInterpreter.value);
+  }
+
   render() {
     if (this.props.data.loading) return <div />;
 
@@ -24,11 +35,14 @@ class ShowSkill extends React.Component {
         <p>{skill.description}</p>
 
         <AceEditor
+          ref="editor"
           mode="javascript"
           theme="tomorrow"
           onChange={onChange}
           name="UNIQUE_ID_OF_DIV"
         />
+
+        <button onClick={this.onRun.bind(this)}>Run</button>
       </div>
     );
   }
