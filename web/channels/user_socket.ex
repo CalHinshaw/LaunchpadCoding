d7591d89d1448c2053912a39e65b9058e70a8ac0
CalHinshaw <calhinshaw@gmail.com>
2017-03-03 14:32:01 -0800

    make changes for heroku

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule Launchpad.UserSocket do
   # channel "room:*", Launchpad.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
