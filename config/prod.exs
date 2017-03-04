da630ed4cce6fe2723d4fcd788f2fc7c856c5ea3
CalHinshaw <calhinshaw@gmail.com>
2017-03-03 17:06:59 -0800

    Fix ecto repo config in prod

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :launchpad, Launchpad.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-config :hello_phoenix, HelloPhoenix.Repo,
+config :launchpad, Launchpad.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
